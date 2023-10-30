ee5c3a028ff8d340d2a4878fcc2ab5cac037606c
jovivaspo <jovivaspo@gmail.com>
2023-10-30 18:29:31 +0100

    Control scroll top

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import { Footer } from "./Footer";
 import { Navbar } from "./Navbar";
 
@@ -10,6 +10,7 @@ export const Layout = () => {
       <div className="mt-24 md:mt-32">
         <Footer />
       </div>
+      <ScrollRestoration />
     </>
   );
 };
