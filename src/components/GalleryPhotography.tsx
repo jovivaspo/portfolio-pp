0e4dc0a6cee0116ac280e20baee4c8c2a55f1df3
jovivaspo <jorgevipo.dev@gmail.com>
2025-11-09 12:15:10 +0100

    Recuperar contenido faltante desde versión desplegada
    - Añadidas 4 imágenes de photography: Thailand x2, Italy, Spain (2024)
    - Actualizada GalleryPhotography con 10 fotos (antes 6)
    - Añadidos 4 videos de YouTube faltantes en Colab
    - Actualizado listVideoDeloittes con 24 videos en orden correcto
    - Total: 24 videos Deloitte + 1 Freelance + 1 Vimeo

--- src/components/GalleryPhotography.tsx
+++ src/components/GalleryPhotography.tsx
@@ -1,72 +1,84 @@
-import Pozo from "../assets/images/pozo.jpg";
-import Bonne from "../assets/images/bonne.jpg";
-import Meteora from "../assets/images/meteora.jpg";
-import FreeGeorgia from "../assets/images/Georgia.jpg";
-import Greek from "../assets/images/Greek.jpg";
-import LLorx from "../assets/images/Llorx.jpg";
-import { ButtonSeeMore } from "./ButtonSeeMore";
-//import { ButtonSeeMore } from "./ButtonSeeMore";
+import Bonne from '../assets/images/bonne.jpg';
+import FreeGeorgia from '../assets/images/Georgia.jpg';
+import Greek from '../assets/images/Greek.jpg';
+import Italy from '../assets/images/Italy.jpg';
+import LLorx from '../assets/images/LLorx.jpg';
+import Meteora from '../assets/images/Meteora.jpg';
+import Pozo from '../assets/images/pozo.jpg';
+import Spain from '../assets/images/spain.jpg';
+import Thailand1 from '../assets/images/Thailand-1.jpg';
+import Thailand from '../assets/images/Thailand.jpg';
+import { ButtonSeeMore } from './ButtonSeeMore';
 
 const items = [
   {
-    href: "https://www.flickr.com/photos/pocostales/53107162640/in/dateposted-public/",
+    href: 'https://www.flickr.com/photos/pocostales/53918163338/in/dateposted-public/',
+    src: Thailand,
+    title: 'Thailand, 2024',
+  },
+  {
+    href: 'https://www.flickr.com/photos/pocostales/53917023307/in/dateposted-public/',
+    src: Thailand1,
+    title: 'Thailand, 2024',
+  },
+  {
+    href: 'https://www.flickr.com/photos/pocostales/53918372780/in/dateposted-public/',
+    src: Italy,
+    title: 'Italy, 2024',
+  },
+  {
+    href: 'https://www.flickr.com/photos/pocostales/53917042917/in/dateposted-public/',
+    src: Spain,
+    title: 'Spain, 2024',
+  },
+  {
+    href: 'https://www.flickr.com/photos/pocostales/53107162640/in/dateposted-public/',
     src: Meteora,
-    title: "Greece, 2023",
+    title: 'Greece, 2023',
   },
   {
-    href: "https://www.flickr.com/photos/pocostales/53106852484/in/dateposted-public/",
+    href: 'https://www.flickr.com/photos/pocostales/53106852484/in/dateposted-public/',
     src: FreeGeorgia,
-    title: "Georgia, 2023",
+    title: 'Georgia, 2023',
   },
   {
-    href: "https://www.flickr.com/photos/pocostales/25548368037/in/dateposted-public/",
-    src: Pozo,
-    title: "Portugal, 2017",
-  },
-  {
-    href: "https://www.flickr.com/photos/pocostales/53106912618/in/dateposted-public/",
-    src: Bonne,
-    title: "Germany, 2022",
-  },
-  {
-    href: "https://www.flickr.com/photos/pocostales/25548365717/in/dateposted-public/",
-    src: LLorx,
-    title: "Spain, 2017",
-  },
-  {
-    href: "https://www.flickr.com/photos/pocostales/53107255178/in/dateposted-public/",
+    href: 'https://www.flickr.com/photos/pocostales/53107255178/in/dateposted-public/',
     src: Greek,
-    title: "Greece, 2023",
+    title: 'Greece, 2023',
+  },
+  {
+    href: 'https://www.flickr.com/photos/pocostales/53106912618/in/dateposted-public/',
+    src: Bonne,
+    title: 'Germany, 2022',
+  },
+  {
+    href: 'https://www.flickr.com/photos/pocostales/25548368037/in/dateposted-public/',
+    src: Pozo,
+    title: 'Portugal, 2017',
+  },
+  {
+    href: 'https://www.flickr.com/photos/pocostales/25548365717/in/dateposted-public/',
+    src: LLorx,
+    title: 'Spain, 2017',
   },
 ];
 
 export const GalleryPhotography = () => {
   return (
     <>
-      <div className="mx-auto flex flex-col sm:grid grid-cols-2 gap-16 mt-36 mb-24 w-[90%] lg:w-[80%] justify-center">
+      <div className='mx-auto flex flex-col sm:grid grid-cols-2 gap-16 mt-36 mb-24 w-[90%] lg:w-[80%] justify-center'>
         {items.map((item, index) => {
           return (
-            <a
-              key={index}
-              target="_blank"
-              href={item.href}
-              className=" w-full h-full flex flex-col gap-4 justify-center items-center"
-            >
-              <img
-                src={item.src}
-                className="w-full h-full object-cover"
-                alt={item.title}
-              />
-              <span className="talic text-my-gray italic text-xs font-semibold">
-                {item.title}
-              </span>
+            <a key={index} target='_blank' href={item.href} className=' w-full h-full flex flex-col gap-4 justify-center items-center'>
+              <img src={item.src} className='w-full h-full object-cover' alt={item.title} />
+              <span className='talic text-my-gray italic text-xs font-semibold'>{item.title}</span>
             </a>
           );
         })}
       </div>
-      <div className="w-full flex justify-center">
-        {" "}
-        <a href="https://www.flickr.com/people/pocostales/" target="_blank">
+      <div className='w-full flex justify-center'>
+        {' '}
+        <a href='https://www.flickr.com/people/pocostales/' target='_blank'>
           {<ButtonSeeMore />}
         </a>
       </div>
--- src/pages/Colab.tsx
+++ src/pages/Colab.tsx
@@ -1,56 +1,60 @@
-import { Header } from "../components/Header";
-import IframeYoutube from "../components/IframeYoutube";
-import { IframeVimeo } from "../components/IframeVimeo";
-import { InstagramCard } from "../components/InstagramCard";
+import { Header } from '../components/Header';
+import { IframeVimeo } from '../components/IframeVimeo';
+import IframeYoutube from '../components/IframeYoutube';
+import { InstagramCard } from '../components/InstagramCard';
 
 const listVideoDeloittes = [
-  "https://www.youtube.com/embed/KkM88VkOh-U?si=IeOgeIvML0baflEP",
-  "https://www.youtube.com/embed/GV4z9GbN_lA?si=mvqxx_PrY9nTkhmG",
-  "https://www.youtube.com/embed/Z3eNm7YTIKc?si=WlfVHHcWCAoaMaOC",
-  "https://www.youtube.com/embed/I_THg7qQD2I?si=1zg87MIZy9FnPQgu",
-  "https://www.youtube.com/embed/ZPU_PM-zmEc?si=nYAWkBzf1ta5SUiO",
-  "https://www.youtube.com/embed/onNR_bSU_xc?si=Lh312vJkLx9oiVbm",
-  "https://www.youtube.com/embed/imidFiPpvsk?si=DZBP3tIJThZ_kSxB",
-  "https://www.youtube.com/embed/W7X1BMFmp2k?si=dZaInHlWKmx481q4",
-  "https://www.youtube.com/embed/syDw6TDn6rE?si=BA5YuaWB8IfikbPk",
-  "https://www.youtube.com/embed/IMGbEFt0Xrs?si=y722lcPELSBXzp6x",
-  "https://www.youtube.com/embed/wIcR96jOw3U?si=jh8SZ1l5DrZPEyhy",
-  "https://www.youtube.com/embed/fj_Vb1lEWBQ?si=r-NacyJ9t93KSRWA",
-  "https://www.youtube.com/embed/4agLySyQDdI?si=ia-bBZBu0PIB1TfW",
-  "https://www.youtube.com/embed/OUSyjRKO0fo?si=0h_Me0SJe8oHOyxp",
-  "https://www.youtube.com/embed/Ps6R6MAyeiY?si=zuNbGEAv8s8DlTd0",
-  "https://www.youtube.com/embed/385R2MXThWs?si=duT_p5SwI56TzH3K",
-  "https://www.youtube.com/embed/e_SVIE7I_j0?si=nui9ELvpNcp_X2WI",
-  "https://www.youtube.com/embed/jOmXGKgnNZE?si=VcfAP_7Wgt0LNs65",
-  "https://www.youtube.com/embed/SRHr_8hfODU?si=n9leuMYBfPnZYHqr",
-  "https://www.youtube.com/embed/dVUhmD3D9hE?si=CpDJ0AHnVzSGKhf4",
+  'https://www.youtube.com/embed/DbqdB3LL_rU',
+  'https://www.youtube.com/embed/JzJ_SLuC8Kg',
+  'https://www.youtube.com/embed/WvF0FkWzcuA',
+  'https://www.youtube.com/embed/11LWXyeejQo',
+  'https://www.youtube.com/embed/KkM88VkOh-U',
+  'https://www.youtube.com/embed/GV4z9GbN_lA',
+  'https://www.youtube.com/embed/Z3eNm7YTIKc',
+  'https://www.youtube.com/embed/I_THg7qQD2I',
+  'https://www.youtube.com/embed/ZPU_PM-zmEc',
+  'https://www.youtube.com/embed/onNR_bSU_xc',
+  'https://www.youtube.com/embed/imidFiPpvsk',
+  'https://www.youtube.com/embed/W7X1BMFmp2k',
+  'https://www.youtube.com/embed/syDw6TDn6rE',
+  'https://www.youtube.com/embed/IMGbEFt0Xrs',
+  'https://www.youtube.com/embed/wIcR96jOw3U',
+  'https://www.youtube.com/embed/fj_Vb1lEWBQ',
+  'https://www.youtube.com/embed/4agLySyQDdI',
+  'https://www.youtube.com/embed/OUSyjRKO0fo',
+  'https://www.youtube.com/embed/Ps6R6MAyeiY',
+  'https://www.youtube.com/embed/385R2MXThWs',
+  'https://www.youtube.com/embed/e_SVIE7I_j0',
+  'https://www.youtube.com/embed/jOmXGKgnNZE',
+  'https://www.youtube.com/embed/SRHr_8hfODU',
+  'https://www.youtube.com/embed/dVUhmD3D9hE',
 ];
 
 const listInstagram = [
   //
   {
-    link: "https://www.instagram.com/p/CmgQW54rA6T/?igshid=MzRlODBiNWFlZA%3D%3D",
-    src: "https://res.cloudinary.com/dme5pqzrj/image/upload/f_auto/v1698441968/portafolio-pp/01_hjnnkn.jpg",
+    link: 'https://www.instagram.com/p/CmgQW54rA6T/?igshid=MzRlODBiNWFlZA%3D%3D',
+    src: 'https://res.cloudinary.com/dme5pqzrj/image/upload/f_auto/v1698441968/portafolio-pp/01_hjnnkn.jpg',
   },
   {
-    link: "https://www.instagram.com/p/ClOvy0SLA10/",
-    src: "https://res.cloudinary.com/dme5pqzrj/image/upload/f_auto/v1698439387/portafolio-pp/02_qvdnio.jpg",
+    link: 'https://www.instagram.com/p/ClOvy0SLA10/',
+    src: 'https://res.cloudinary.com/dme5pqzrj/image/upload/f_auto/v1698439387/portafolio-pp/02_qvdnio.jpg',
   },
   {
-    link: "https://www.instagram.com/p/ClT0S2Kp7cS/",
-    src: "https://res.cloudinary.com/dme5pqzrj/image/upload/f_auto/v1698439800/portafolio-pp/03_dpgelm.jpg",
+    link: 'https://www.instagram.com/p/ClT0S2Kp7cS/',
+    src: 'https://res.cloudinary.com/dme5pqzrj/image/upload/f_auto/v1698439800/portafolio-pp/03_dpgelm.jpg',
   },
   {
-    link: "https://www.instagram.com/p/CXL5NZ7sDW_/",
-    src: "https://res.cloudinary.com/dme5pqzrj/image/upload/f_auto/v1698439752/portafolio-pp/09_ivpl9g.jpg",
+    link: 'https://www.instagram.com/p/CXL5NZ7sDW_/',
+    src: 'https://res.cloudinary.com/dme5pqzrj/image/upload/f_auto/v1698439752/portafolio-pp/09_ivpl9g.jpg',
   },
   {
-    link: "https://www.instagram.com/p/B44mkyGjIz2/",
-    src: "https://res.cloudinary.com/dme5pqzrj/image/upload/f_auto/v1698439979/portafolio-pp/14_k7g982.jpg",
+    link: 'https://www.instagram.com/p/B44mkyGjIz2/',
+    src: 'https://res.cloudinary.com/dme5pqzrj/image/upload/f_auto/v1698439979/portafolio-pp/14_k7g982.jpg',
   },
   {
-    link: "https://www.instagram.com/p/CIGACdbAOao/",
-    src: "https://res.cloudinary.com/dme5pqzrj/image/upload/f_auto/v1698440056/portafolio-pp/11_auzleb.jpg",
+    link: 'https://www.instagram.com/p/CIGACdbAOao/',
+    src: 'https://res.cloudinary.com/dme5pqzrj/image/upload/f_auto/v1698440056/portafolio-pp/11_auzleb.jpg',
   },
 ];
 
@@ -58,56 +62,46 @@ export const Colab = () => {
   return (
     <>
       <Header
-        position="0% 50%"
-        text="Feat/Collaboration"
-        image="https://res.cloudinary.com/dme5pqzrj/image/upload/f_auto/t_header-feat-collaboration-crop/v1698424080/portafolio-pp/header-feat-collaboration_zahc6o.jpg"
+        position='0% 50%'
+        text='Feat/Collaboration'
+        image='https://res.cloudinary.com/dme5pqzrj/image/upload/f_auto/t_header-feat-collaboration-crop/v1698424080/portafolio-pp/header-feat-collaboration_zahc6o.jpg'
       />
 
-      <div className="w-full flex flex-col justify-center items-center mt-32">
-        <div className="my-2 flex flex-col gap-2 px-8">
-          <p className="text-center">
+      <div className='w-full flex flex-col justify-center items-center mt-32'>
+        <div className='my-2 flex flex-col gap-2 px-8'>
+          <p className='text-center'>
             <b>FREELANCE</b>
           </p>
-          <p className="text-center text-my-gray">
-            External collaboration taking on a supporting role in these
-            audiovisual projects.{" "}
-          </p>
+          <p className='text-center text-my-gray'>External collaboration taking on a supporting role in these audiovisual projects. </p>
         </div>
-        <div className="mx-auto flex flex-col sm:grid grid-cols-2 gap-16 md:gap-24 mt-24 w-[90%] lg:w-[80%] justify-center">
-          <IframeYoutube src="https://www.youtube.com/embed/sqH7l-3UNM0?si=c9aSOeDclfbeJ17-" />
-          <IframeVimeo src="https://player.vimeo.com/video/297813109?h=624d13a6c3" />
+        <div className='mx-auto flex flex-col sm:grid grid-cols-2 gap-16 md:gap-24 mt-24 w-[90%] lg:w-[80%] justify-center'>
+          <IframeYoutube src='https://www.youtube.com/embed/sqH7l-3UNM0?si=c9aSOeDclfbeJ17-' />
+          <IframeVimeo src='https://player.vimeo.com/video/297813109?h=624d13a6c3' />
         </div>
       </div>
-      <div className="w-full flex flex-col justify-center items-center mt-32 px-8">
-        <div className="my-2 flex flex-col gap-2">
-          <p className="text-center">
+      <div className='w-full flex flex-col justify-center items-center mt-32 px-8'>
+        <div className='my-2 flex flex-col gap-2'>
+          <p className='text-center'>
             <b>DELOITTE</b>
           </p>
-          <p className="text-center text-my-gray">
-            Selection of the best videos created in collaboration with the
-            Deloitte Brand and Content team. My contribution as film maker,
-            video editor and motion designer.
+          <p className='text-center text-my-gray'>
+            Selection of the best videos created in collaboration with the Deloitte Brand and Content team. My contribution as film maker, video editor and motion designer.
           </p>
         </div>
-        <div className="mx-auto flex flex-col sm:grid grid-cols-2 gap-16 md:gap-24 mt-24 w-[90%] lg:w-[80%] justify-center">
+        <div className='mx-auto flex flex-col sm:grid grid-cols-2 gap-16 md:gap-24 mt-24 w-[90%] lg:w-[80%] justify-center'>
           {listVideoDeloittes.map((item, index) => {
             return <IframeYoutube key={index} src={item} />;
           })}
         </div>
-        <div className="my-2 flex flex-col gap-2 mt-32 px-8">
-          <p className="text-center">
+        <div className='my-2 flex flex-col gap-2 mt-32 px-8'>
+          <p className='text-center'>
             <b>DELOITTE - INSTAGRAM</b>
           </p>
-          <p className="text-center text-my-gray">
-            Samples of social media content created in collaboration with the
-            Deloitte Brand and Content team.
-          </p>
+          <p className='text-center text-my-gray'>Samples of social media content created in collaboration with the Deloitte Brand and Content team.</p>
         </div>
-        <div className="flex w-full justify-center px-8 lg:w-[90%] 2xl:[80%] gap-16 flex-wrap mt-24">
+        <div className='flex w-full justify-center px-8 lg:w-[90%] 2xl:[80%] gap-16 flex-wrap mt-24'>
           {listInstagram.map((item, index) => {
-            return (
-              <InstagramCard key={index} src={item.src} link={item.link} />
-            );
+            return <InstagramCard key={index} src={item.src} link={item.link} />;
           })}
         </div>
       </div>
